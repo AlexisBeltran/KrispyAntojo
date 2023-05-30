eb50f2cd950f1cb8b8f2bfe69fbd464e29c720e0
AlexisBeltran <jesus_beltran011@outlook.com>
2023-05-30 17:52:47 -0600

    Test

--- includes/app.php
+++ includes/app.php
@@ -1,6 +1,7 @@
 <?php
     require 'funciones.php';
     require 'config/database.php';
+    Debuguear("Hola");
     require __DIR__ . '/../vendor/autoload.php';
     $db = conectarDB();
     use Model\ActiveRecord;
