706c257456fb555e3d497b8c09d47644e3a555f6
AlexisBeltran <jesus_beltran011@outlook.com>
2023-05-30 17:46:30 -0600

    Debug

--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,8 @@
 <?php 
+
     require_once __DIR__ . "/../includes/app.php";
+    Debuguear(__DIR__ . "/../includes/app.php");
+
     use MVC\Router;    
     use Controllers\loginControllers;
     use Controllers\usuarioControllers;
