cbecc45388ae5b53d840fbf86fe0b35057123868
AlexisBeltran <jesus_beltran011@outlook.com>
2023-06-01 18:34:22 -0600

    ROUTER V9

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -126,7 +126,7 @@
             public function autenticar(){
                 $_SESSION['login'] = true;
                 session_start();
-                header('Location: /Public/index.php/Dashboard/dashboard?View=1');
+                header('Location: Public/index.php/Dashboard/dashboard?View=1');
 
             }
             
